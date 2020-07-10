de4cc40eff4dad11eeb071c03062033734cde3b8
何好听 <monstercodings@163.com>
2020-07-10 23:56:10 +0800

    jdk14

--- src/test/java/top/codings/websiphon/test/NewHttpTest.java
+++ src/test/java/top/codings/websiphon/test/NewHttpTest.java
@@ -6,15 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.junit.jupiter.api.Test;
 
-import javax.net.ssl.SSLContext;
-import java.net.InetSocketAddress;
-import java.net.ProxySelector;
+import javax.net.ssl.*;
+import java.net.Socket;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
@@ -24,24 +23,26 @@ public class NewHttpTest {
     @SneakyThrows
     @Test
     public void test() {
+        SSLContext sslContext = SSLContextBuilder.create().loadTrustMaterial((x509Certificates, s) -> true).build();
+        sslContext.init(null, MyTrustManager.get(), null);
         HttpClient client = HttpClient.newBuilder()
                 .executor(Executors.newSingleThreadExecutor())
                 .connectTimeout(Duration.ofSeconds(6))
                 .version(HttpClient.Version.HTTP_2)
                 .followRedirects(HttpClient.Redirect.NORMAL)
-                .sslContext(SSLContextBuilder.create().loadTrustMaterial((x509Certificates, s) -> true).build())
+                .sslContext(sslContext)
+                .sslParameters(new SSLParameters())
 //                .proxy(ProxySelector.of(new InetSocketAddress("127.0.0.1", 1080)))
 //                .authenticator(Authenticator.getDefault())
                 .build();
         JSONObject json = new JSONObject();
-        json.put("phone", "13826531204");
-        json.put("passwd", "hejian");
+        json.put("phone", "1");
+        json.put("passwd", "1");
         HttpRequest request = HttpRequest.newBuilder()
                 /*.uri(URI.create("https://auth.glli.top:30001/auth/vpn/pc/login"))
                 .POST(HttpRequest.BodyPublishers.ofString(json.toJSONString(), Charset.forName("utf-8")))
                 .header("Content-Type", "application/json")*/
-                .uri(URI.create("https://codings.top"))
-                .timeout(Duration.ofSeconds(30))
+                .uri(URI.create("https://vdash.codings.top:7921"))
                 .build();
         int total = 1;
         CompletableFuture[] futures = new CompletableFuture[total];
@@ -49,7 +50,7 @@ public class NewHttpTest {
             futures[i] = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                     .whenCompleteAsync((stringHttpResponse, throwable) -> {
                         if (throwable != null) {
-                            log.error("发生异常 -> {}", throwable.getMessage());
+                            log.error("发生异常 -> {}", throwable.getMessage(), throwable);
                             return;
                         }
                         log.debug(stringHttpResponse.body());
@@ -61,5 +62,46 @@ public class NewHttpTest {
         } catch (Exception e) {
 
         }
+    }
+}
+
+class MyTrustManager extends X509ExtendedTrustManager {
+    static TrustManager[] get() {
+        return new TrustManager[]{new MyTrustManager()};
+    }
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket) throws CertificateException {
+
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) throws CertificateException {
+
+    }
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine) throws CertificateException {
+
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine engine) throws CertificateException {
+
+    }
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+    }
+
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
     }
 }
